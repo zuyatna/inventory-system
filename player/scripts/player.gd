b62809695e695704dd285c444f702401489fa8fe
Suyatna <zuyatna@gmail.com>
2024-06-10 18:44:06 +0700

    Adding inventory item

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,11 +1,36 @@
 extends CharacterBody2D
 
+@export var speed = 200
 
-# Called when the node enters the scene tree for the first time.
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _ready():
-	pass # Replace with function body.
+	Global.set_player_reference(self)
 
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	update_animation()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_input():
+	var horizontal_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
+	var vertical_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up");
+	var movement_vector: Vector2 = Vector2(horizontal_movement, vertical_movement)
+	var direction: Vector2 = movement_vector.normalized()
+	
+	velocity = direction * speed
+
+func update_animation():
+	if velocity == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				animated_sprite.play("walk_right")
+			else:
+				animated_sprite.play("walk_left")
+		else:
+			if velocity.y > 0:
+				animated_sprite.play("walk_down")
+			else:
+				animated_sprite.play("walk_up")
